3b2ccc46405da674e3d86189c1330ad0bd798290
sgomez-p <sergiogomezprados@gmail.com>
2023-04-25 13:49:15 +0200

    first try

---
+++ src/com/company/suma.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class suma {
+    public static void main(String[] args) {
+        System.out.println(suma(1, 3));
+    }
+
+    public static int suma(int n1, int n2)
+        {
+            int suma;
+            suma = n1 + n2;
+
+            return suma;
+
+        }
+}
+
+
+
